cecd2c3f33209e9e0710a3224fcf20ba55ff1ac1
valentina buoro <valentinabuoro4@gmail.com>
2023-12-07 21:27:48 +0100

    create vote

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -1,36 +1,76 @@
-import { Voltaire } from 'next/font/google'
-import Image from 'next/image'
-import React from 'react'
-import Logo from '../../public/logo.svg'
+import { Voltaire } from "next/font/google";
+import Image from "next/image";
+import React from "react";
+import Logo from "../../public/logo.svg";
+import Phone from "../../public/phone.svg";
+import Vote from "../../public/vote.svg";
+import Facebook from "../../public/facebook.svg";
+import Youtube from "../../public/youtube.svg";
+import Linkedin from "../../public/linkedin.svg";
+import Twitter from "../../public/twitter.svg";
 
 const Footer = () => {
   return (
-    <div className='px-8 lg:px-20'>
-        <div className='flex items-center justify-center border-b border-[#D9D9D9] pb-10 pt-6'>
-        <span className='text-[#3366CC] font-black flex md:text-[28.777px] md:leading-[ 25.899px] items-center justify-center'><span className='w-[40px] h-[40px] md:w-[90px] md:h-[90px] flex items-center justify-end'><Image src={Logo} alt="logo"  /> </span><span>VoteMe</span></span>
+    <div className="px-8 lg:px-20">
+      <div className="flex items-center justify-center border-b border-[#D9D9D9] pb-10 pt-6">
+        <span className="text-[#3366CC] font-black flex md:text-[28.777px] md:leading-[ 25.899px] items-center justify-center">
+          <span className="w-[40px] h-[40px] md:w-[90px] md:h-[90px] flex items-center justify-end">
+            <Image src={Logo} alt="logo" />{" "}
+          </span>
+          <span>VoteMe</span>
+        </span>
+      </div>
+      <div className="grid grid-cols-3 gap-3 md:gap-5 lg:px-10 py-14">
+        <div>
+          <p className="md:text-xl text-xs font-semibold ">
+            Banana Island, Ikoyi, Lagos State, Nigeria
+          </p>
         </div>
-        <div className='grid grid-cols-3 gap-5 lg:px-10 py-14'>
-            <div>
-                <p className='md:text-xl text-xs font-semibold '>Banana Island, Ikoyi, Lagos State, Nigeria</p>
-            </div>
-            <div className='md:text-xl text-xs font-medium flex flex-col space-x-4 md:space-y-10'>
-                <p>Home</p>
-                <p>Verification Process</p>
-                <p>Vote</p>
-            </div>
-            <div className='md:text-xl text-xs font-medium flex flex-col space-y-8'>
-                <p > +234 815 456 7890</p>
-                <p>voteme@gmail.com</p>
-            </div>
+        <div className="md:text-xl text-xs font-medium flex flex-col items-start space-y-3 md:space-y-10">
+          <p>Home</p>
+          <p>Verification Process</p>
+          <p>Vote</p>
         </div>
-        <div className='rounded-lg text-xs md:text-sm lg:rounded-3xl bg-[#D9D9D9] py-3 px-2 lg:py-6 lg:px-5'>
-            <div>
-                <span>Site by VoteMe</span>
-                <span>© 2023 - VoteMe. All rights reserved.</span>
-            </div>
-        </div>
-    </div>
-  )
-}
+        <div className="md:text-xl text-xs tracking-tighter md:tracking-normal font-medium flex flex-col space-y-3 md:space-y-8">
+          <p className="flex">
+            {" "}
+            <span className="w-4 md:w-5 mr-2">
+              {" "}
+              <Image src={Phone} alt="hello" className="w-full" />
+            </span>{" "}
+            <span>+234 815 456 7890</span>
+          </p>
+          <p className="flex">
+            <span className="w-4 md:w-5 mr-2">
+              <Image src={Vote} alt="hello" className="w-full" />
+            </span>
+            <span>voteme@gmail.com</span>
+          </p>
 
-export default Footer
+          <div className="flex space-x-3 items-center">
+            <span className="w-4 md:w-5 mr-2">
+              <Image src={Facebook} alt="hello" className="w-full" />
+            </span>
+            <span className="w-4 md:w-5 mr-2">
+              <Image src={Youtube} alt="hello" className="w-full" />
+            </span>
+            <span className="w-4 md:w-5 mr-2">
+              <Image src={Linkedin} alt="hello" className="w-full" />
+            </span>
+            <span className="w-4 md:w-5 mr-2">
+              <Image src={Twitter} alt="hello" className="w-full" />
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-lg text-xs md:text-sm lg:rounded-3xl bg-[#D9D9D9] py-3 px-2 lg:py-6 lg:px-5">
+        <div>
+          <span>Site by VoteMe</span>
+          <span>© 2023 - VoteMe. All rights reserved.</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/app/(group)/election/vote/page.tsx
@@ -0,0 +1,113 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+//import { Provider } from 'ethers';
+import { contractAddress, contractAbi } from "../../../../_constants/constant";
+import Navbar from "@/_components/navbar";
+import Footer from "@/_components/footer";
+
+const Page = (props: any) => {
+  const [provider, setProvider] = useState({});
+  const [account, setAccount] = useState("");
+  const [connected, setConnected] = useState(false);
+
+  async function connectToMetamask() {
+    /*if (window.ethereum) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        console.log("Metamask Connected : " + address);
+        setConnected(true);
+      } 
+      catch (error) {
+        console.log(error);
+      }
+    } else{
+      alert("Please install metamask");
+    }*/
+  }
+
+  return (
+    /*<div>
+      <h1>Welcome to decentralized voting</h1>
+      <button onClick={connectToMetamask}>Click me</button>
+    </div>*/
+    <>
+    <Navbar/>
+    <div className=" justify-between  p-8 lg:p-20 bg-[#F8F3F3]">
+      <div className="mx-auto w-[300px] md:w-[460px]  ">
+        <div className="mb-6 md:mb-8 lg:mb-10 text-center">
+          <p className=" text-2xl md:3xl lg:text-4xl font-bold"> Vote</p>
+        </div>
+        <div className="mb-4 md:mb-6">
+          <label
+            className="font-medium text-base md:text-[18px] text-[#0D0D0D] mb-2"
+            htmlFor="title"
+          >
+            Who is your preferred presidential candiadte?
+          </label>
+          <div className="flex justify-between p-2 md:p-4 w-full rounded-lg border border-[#666666] ">
+            <input
+              className="bg-inherit w-full border-none outline-none placeholder:text-sm"
+              placeholder="Enter your answer"
+              id="title"
+              name="title"
+              value={""}
+              onChange={() => {}}
+            />
+          </div>
+        </div>
+        <div  className="mb-4 md:mb-6">
+          <label
+            className="font-medium text-base md:text-[18px] text-[#0D0D0D] mb-2"
+            htmlFor="title"
+          >
+            Reason for choosing him?
+            
+          </label>
+          <div className="flex justify-between p-2 md:p-4 w-full rounded-lg border border-[#666666] ">
+            <input
+              className="bg-inherit w-full border-none outline-none placeholder:text-sm"
+              placeholder="Enter your answer"
+              id="title"
+              name="title"
+              value={""}
+              onChange={() => {}}
+            />
+          </div>
+        </div>
+        <div  className="mb-4 md:mb-6">
+          <label
+            className="font-medium text-base md:text-[18px] text-[#0D0D0D] mb-2"
+            htmlFor="title"
+          >
+            What will you want me to do for the country?
+          </label>
+          <div className="flex justify-between p-2 md:p-4 w-full h-[120px] rounded-lg border border-[#666666] ">
+            <input
+              className="bg-inherit w-full border-none outline-none placeholder:text-sm"
+              placeholder="Enter your questions"
+              id="title"
+              name="title"
+              value={""}
+              onChange={() => {}}
+            />
+          </div>
+        </div>
+       
+        
+        <button className="rounded-[10px] text-[#F6F4F4] bg-[#001F3F] w-full  py-3 md:py-4  ">
+         
+         Cast Vote
+        </button>
+      </div>
+    </div>
+    <Footer/></>
+  );
+};
+
+export default Page;
